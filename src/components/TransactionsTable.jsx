780d3a0774b28d7cada73120ca6871e00805d531
Sam Tomashi <samtomashikayembi@yahoo.fr>
2025-04-08 15:15:11 +0300

    define components

---
+++ src/components/TransactionsTable.jsx
@@ -0,0 +1,16 @@
+export default function TransactionsTable(){
+
+    return(
+        <table className="table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+        </table>
+    )
+
+}
